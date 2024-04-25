7efeb696829a0d4be1863fe9655cee10d9cec0b3
WolfGGO <klimenkomisa1@gmail.com>
2024-04-25 09:40:15 +0300

    Добавьте файлы проекта.

---
+++ лр16/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace лр16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Заводы по производству автомобилей";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Седан";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(290, 62);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Создать";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Внедорожник";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(192, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 25);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Лимузин";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(328, 182);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "ЛР16";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ лр16/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр16
+{
+    public partial class Form1 : Form
+    {
+        // Абстрактный класс для автомобилей
+        abstract class Car
+        {
+            // Абстрактный метод для управления автомобилем
+            public abstract void Drive();
+        }
+
+        // Класс для седана
+        class Sedan : Car
+        {
+            // Реализация метода Drive() для седана
+            public override void Drive()
+            {
+                MessageBox.Show("Произведен седан");
+            }
+        }
+
+        // Класс для внедорожника (SUV)
+        class SUV : Car
+        {
+            // Реализация метода Drive() для внедорожника
+            public override void Drive()
+            {
+                MessageBox.Show("Произведен внедорожник");
+            }
+        }
+
+        // Класс для лимузина
+        class Limousine : Car
+        {
+            // Реализация метода Drive() для лимузина
+            public override void Drive()
+            {
+                MessageBox.Show("Произведен лимузин");
+            }
+        }
+
+        // Абстрактная фабрика для создания автомобилей
+        abstract class AbstractFactory
+        {
+            // Абстрактный метод для создания автомобиля
+            public abstract Car CreateCar();
+        }
+
+        // Фабрика для производства седанов
+        class SedanFactory : AbstractFactory
+        {
+            // Реализация метода CreateCar() для создания седана
+            public override Car CreateCar()
+            {
+                return new Sedan();
+            }
+        }
+
+        // Фабрика для производства внедорожников
+        class SUVFactory : AbstractFactory
+        {
+            // Реализация метода CreateCar() для создания внедорожника
+            public override Car CreateCar()
+            {
+                return new SUV();
+            }
+        }
+
+        // Фабрика для производства лимузинов
+        class LimousineFactory : AbstractFactory
+        {
+            // Реализация метода CreateCar() для создания лимузина
+            public override Car CreateCar()
+            {
+                return new Limousine();
+            }
+        }
+
+        // Конструктор формы
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик события нажатия кнопки "Создать седан"
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Создание фабрики для седанов
+            AbstractFactory sedanFactory = new SedanFactory();
+            // Создание седана через фабрику
+            Car car = sedanFactory.CreateCar();
+            // Управление седаном
+            car.Drive();
+        }
+
+        // Обработчик события нажатия кнопки "Создать внедорожник"
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Создание фабрики для внедорожников
+            AbstractFactory suvFactory = new SUVFactory();
+            // Создание внедорожника через фабрику
+            Car car = suvFactory.CreateCar();
+            // Управление внедорожником
+            car.Drive();
+        }
+
+        // Обработчик события нажатия кнопки "Создать лимузин"
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Создание фабрики для лимузинов
+            AbstractFactory limousineFactory = new LimousineFactory();
+            // Создание лимузина через фабрику
+            Car car = limousineFactory.CreateCar();
+            // Управление лимузином
+            car.Drive();
+        }
+    }
+}
